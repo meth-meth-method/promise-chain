16f3ef63bad5b2405a1d0a56568c9f0db65e99f6
Pontus Persson <pontus.alexander@gmail.com>
2016-08-21 21:14:18 +0200

    Add setTimeout in step3 that calls step4.

--- main.js
+++ main.js
@@ -9,7 +9,15 @@ function step2() {
 }
 
 function step3() {
+    setTimeout(step4, 0);
+
+    for (let i = 1000000000; --i;);
+
     console.trace('step3');
+}
+
+function step4() {
+    console.trace('step4');
 }
 
 step1();
